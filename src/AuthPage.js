6d568e4a16476b363bd2a7876fd648737a8df2b4
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-12 17:08:24 -0700

    set state for AuthPage

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils';
 
-export default function AuthPage() {
+export default function AuthPage({ setEmail, setToken, getUser }) {
+
+  const [formEmail, setFormEmail] = useState('');
+  const [formPassword, setFormPassword] = useState('');
+  
   return (
     <div>AuthPage</div>
   );
