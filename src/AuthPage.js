8777ef87cbbec37bfac8e409881048b98ede894f
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-12 17:12:24 -0700

    wrote handle functions for signin signup forms

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -6,6 +6,22 @@ export default function AuthPage({ setEmail, setToken, getUser }) {
 
   const [formEmail, setFormEmail] = useState('');
   const [formPassword, setFormPassword] = useState('');
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    await signUp(formEmail, formPassword);
+    const user = await getUser();
+    setEmail(user.user.email);
+    setToken(user.access_token);
+  }
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    await signIn(formEmail, formPassword);
+    const user = getUser();
+    setEmail(user.email);
+    setToken(user.access_token);
+  }
   
   return (
     <div>AuthPage</div>
