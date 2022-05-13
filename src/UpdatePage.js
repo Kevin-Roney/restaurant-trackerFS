4f00bdefab53d8193d3d6fb35b48adea5ce23698
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-13 09:11:10 -0700

    set stat for UpdatePage

--- src/UpdatePage.js
+++ src/UpdatePage.js
@@ -3,6 +3,18 @@ import { useParams, useHistory } from 'react-router-dom';
 import { getRestaurantById } from './services/fetch-utils';
 
 export default function UpdatePage() {
+
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [restaurantInForm, setRestaurantInForm] = useState({
+    name: '',
+    location: '',
+    cuisine: '',
+    yelp_score: 0,
+    google_score: 0,
+    kid_friendly: false,
+  });
+  
   return (
     <div>UpdatePage</div>
   );
