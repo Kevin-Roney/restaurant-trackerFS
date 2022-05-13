12cd8f32c0457dbd188db2ca9d1787f57120696f
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-12 17:46:49 -0700

    set state for CreatePage

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -3,6 +3,17 @@ import { useHistory } from 'react-router-dom';
 import { createRestaurant } from './services/fetch-utils';
 
 export default function CreatePage() {
+
+  const history = useHistory();
+  const [restaurantInForm, setRestaurantInForm] = useState({
+    name: '',
+    location: '',
+    cuisine: '',
+    yelp_score: 0,
+    google_score: 0,
+    kid_friendly: false,
+  });
+  
   return (
     <div>CreatePage</div>
   );
